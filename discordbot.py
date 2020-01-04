03206eb867bf517d985f6f2c7e17f11436bad3c7
sirokuma10121airando <57626493+sirokuma10121airando@users.noreply.github.com>
2020-01-05 01:23:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -337,32 +337,33 @@ async def on_message(message):
 async def on_message_edit(before,after):
     global edit_flag
     if edit_flag == True:
-        edit_flag=False
         if after.channel == t_ch and t_flag == True and after.embeds[0].description and before.embeds != after.embeds:
+            edit_flag=False
+            
             if "正解" in after.embeds[0].description:
                 await t_ch.send("::t Training")
 
-        if after.embeds and after.embeds[0].description:
-            if f"{client.user.mention}はレベルアップした！" in after.embeds[0].description:
-                dateTime = datetime.now(JST)
-                lv = after.embeds[0].description.split("`")[1]
-                embed = discord.Embed(
-                    title = "━<:Lv:643122451500367902><:UP:643122445213106176>━",
-                    description = f"**__{lv}__**",
-                    color = discord.Color.green())
-                embed.set_footer(text = f"{dateTime.year}年{dateTime.month}月{dateTime.day}日　{dateTime.hour}時{dateTime.minute}分{dateTime.second}秒")
-                [await c.send(embed=embed) for c in client.get_all_channels() if c.name == "yuiレベルアップログ"]
-                log_embed = discord.Embed(
-                    title = "━<:Lv:643122451500367902><:UP:643122445213106176>━",
-                    description = f"**__{lv}__**",
-                    color = discord.Color.green())
-                log_embed.add_field(name = "**‣LvUP鯖Name**" ,value = f"『{after.guild.name}』",inline = False)
-                log_embed.add_field(name = "**‣LvUP鯖ID**" ,value = f"『{after.guild.id}』",inline = False)
-                log_embed.add_field(name = "**‣LvUPチャンネルName**" ,value = f"『{after.channel.name}』",inline = False)
-                log_embed.add_field(name = "**‣LvUPチャンネルID**" ,value = f"『{after.channel.id}』",inline = False)
-                log_embed.set_footer(text = f"{dateTime.year}年{dateTime.month}月{dateTime.day}日　{dateTime.hour}時{dateTime.minute}分{dateTime.second}秒")
-                lvlog_ch = client.get_channel(660480078513307659)
-                await lvlog_ch.send(embed = log_embed)
+            if after.embeds and after.embeds[0].description:
+                if f"{client.user.mention}はレベルアップした！" in after.embeds[0].description:
+                    dateTime = datetime.now(JST)
+                    lv = after.embeds[0].description.split("`")[1]
+                    embed = discord.Embed(
+                        title = "━<:Lv:643122451500367902><:UP:643122445213106176>━",
+                        description = f"**__{lv}__**",
+                        color = discord.Color.green())
+                    embed.set_footer(text = f"{dateTime.year}年{dateTime.month}月{dateTime.day}日　{dateTime.hour}時{dateTime.minute}分{dateTime.second}秒")
+                    [await c.send(embed=embed) for c in client.get_all_channels() if c.name == "yuiレベルアップログ"]
+                    log_embed = discord.Embed(
+                        title = "━<:Lv:643122451500367902><:UP:643122445213106176>━",
+                        description = f"**__{lv}__**",
+                        color = discord.Color.green())
+                    log_embed.add_field(name = "**‣LvUP鯖Name**" ,value = f"『{after.guild.name}』",inline = False)
+                    log_embed.add_field(name = "**‣LvUP鯖ID**" ,value = f"『{after.guild.id}』",inline = False)
+                    log_embed.add_field(name = "**‣LvUPチャンネルName**" ,value = f"『{after.channel.name}』",inline = False)
+                    log_embed.add_field(name = "**‣LvUPチャンネルID**" ,value = f"『{after.channel.id}』",inline = False)
+                    log_embed.set_footer(text = f"{dateTime.year}年{dateTime.month}月{dateTime.day}日　{dateTime.hour}時{dateTime.minute}分{dateTime.second}秒")
+                    lvlog_ch = client.get_channel(660480078513307659)
+                    await lvlog_ch.send(embed = log_embed)
 
         await asyncio.sleep(0.5)
         edit_flag = True
