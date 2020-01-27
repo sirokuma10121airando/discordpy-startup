a2927b92e9739fd7d8264c724e50b8b8585e4782
sirokuma10121airando <57626493+sirokuma10121airando@users.noreply.github.com>
2020-01-28 02:09:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -270,7 +270,7 @@ async def on_message(message):
     if message.content == 's!rmap':
         await message.channel.send('::rmap')
 
-    if message.content.startswith('s!sinka '):
+    if message.content.startswith('s!sinka0 '):
         await message.channel.send('::sinka')
         reaction=message.content.split(' ')[1]
         def role_check(tao_msg):
